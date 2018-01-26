751597be16ac096690e25629cc25a4082323551b
Geoffrey Clark <g9clark@ws.cs.ryerson.ca>
2018-01-25 20:57:02 -0500

    VowelComparator progress

--- VowelComparator.java
+++ VowelComparator.java
@@ -1,8 +1,16 @@
 import java.util.*;
 
 public class VowelComparator implements Comparator<String>{
-
+  // private int s1count = 0;
+  // private int s2count = 0;
+  private int[] count = new int[2];
   @Override public int compare(String s1, String s2){
-
+    for(int i=0; i<count.length; i++){
+      for(char ch : s1.toCharArray()){
+        if(ch == 'a' || ch == 'e' || ch == 'i'|| ch == 'o' || ch == 'u'){
+           count[i] += 1;
+        }
+      }
+    }
   }
 }
