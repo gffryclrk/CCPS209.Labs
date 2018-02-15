daee82929be8f542d0c64adc5c01068797b0ff0e
Geoffrey Clark <geoffrey.clark@RUAD.RYERSON.CA>
2018-02-15 16:28:34 -0500

    Started Lab 04: UnixCommands

---
+++ UnixCommands.java
@@ -0,0 +1,48 @@
+//import java.io.Reader;
+//import java.io.FileReader;
+//import java.io.FileWriter;
+//import java.io.IOException;
+import java.io.*;
+
+public class UnixCommands {
+	public static int[] wc(Reader in) throws IOException{
+		
+		//FileReader inputStream = null;
+		//FileWriter outputStream  = null;
+		BufferedReader br = new BufferedReader(in);
+		
+		int[] counts = new int[3];
+		
+		//try {
+			
+			int c;
+			//char c;
+			while((c = br.read()) != -1) {
+				counts[2] += 1;
+				if(Character.isWhitespace(c)) counts[1] += 1;
+				//if(int =)
+				//if('\n') counts[2] +=1;
+				char b = (char) c;
+				//System.out.println(c);
+				
+			}
+			
+			
+		//} finally {
+			//if (br != null) {
+				//br.flush();
+			//}
+			System.out.println("0 " + counts[0] + " 1 " + counts[1] + " 2 " + counts[2]);
+			return counts;
+		//}
+		
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		Reader r = new InputStreamReader(new FileInputStream("README.TXT"), "UTF-8");
+		wc(r);
+		r.close();
+	}
+	
+}
