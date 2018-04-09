92c4e8756567800fc8bb398ddacf7dfe57f03f32
gffryclrk <mr@gffryclrk.com>
2018-04-09 07:18:09 -0400

    Updated Lab 07: Anagrams. Fundamental Theorem of Arithmetic

--- Anagrams.java
+++ Anagrams.java
@@ -4,12 +4,13 @@ import java.io.File;
 import java.util.*;
 
 public class Anagrams {
-	private Map<String,ArrayList<String>> hm = new HashMap<String,ArrayList<String>>();
+	private Map<Long,ArrayList<String>> hm = new HashMap<Long,ArrayList<String>>();
 	
 	public Anagrams(String[] wordlist) {			
 		
 		for(String word: wordlist) {
-			String hc = sortChars(word);
+			// String hc = sortChars(word);
+			Long hc = calcProd(word);
 			ArrayList<String> lv = hm.get(hc);
 			if(lv == null) { lv = new ArrayList<String>(1); }
 			lv.add(word);
@@ -19,13 +20,13 @@ public class Anagrams {
 	}
 /*	public AnagramStream(String[] wordlist) {
 		List<String> al = Arrays.asList(wordlist);
-//		al.forEach(word -> {
-//			Integer hc = sortChars(word).hashCode();
-//			ArrayList<String> lv = hm.get(hc);
-//			if(lv == null) { lv = new ArrayList<String>(1); }
-//			lv.add(word);
-//			hm.put(hc, lv);
-//		});
+		al.forEach(word -> {
+			Integer hc = sortChars(word).hashCode();
+			ArrayList<String> lv = hm.get(hc);
+			if(lv == null) { lv = new ArrayList<String>(1); }
+			lv.add(word);
+			hm.put(hc, lv);
+		});
 		
 		Map<Integer, ArrayList<String>> hm = al.stream()
 		.map(x -> new HashMap<Integer, ArrayList<String>>(sortChars(x).hashCode(), new ArrayList<String>(x))
@@ -46,12 +47,24 @@ public class Anagrams {
 */
 	public String[] allAnagrams(String word) {
 //		https://stackoverflow.com/questions/5374311/convert-arrayliststring-to-string-array
-		String hc = sortChars(word);
+		// String hc = sortChars(word);
+		Long hc = calcProd(word);
 		ArrayList<String> al = hm.get(hc);
 		if(al == null) return new String[0];
 		else {
-			return al.toArray(new String[0]);
+			// return al.toArray(new String[0]);
+			return al;
 		}
+	}
+	private long calcProd(String w){
+		// https://stackoverflow.com/questions/12477339/finding-anagrams-for-a-given-word
+		private int[] PRIMES = new int[] {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,83,89,97,101,103,107,109,113};
+		private long count = 1;
+		// char[] ca = w.toCharArray();
+		for(char c : w.toCharArray()){
+			count *= PRIMES[(int)c - 97];
+		}
+		return count;
 	}
 	private String sortChars(String word) {
 //		https://stackoverflow.com/questions/30199304/sorting-characters-alphabetically-in-a-string
